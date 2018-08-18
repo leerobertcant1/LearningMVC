a225a43b6b9e37dcbab335147e4d6883693fc0eb
Lee Cant <leerobertcant@gmail.com>
2018-08-18 12:53:25 +0100

    Add project files.

---
+++ LearningMVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using LearningMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearningMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            AboutModel model = new AboutModel();
+            model.Name = "Lee Cant";
+            model.Location = "Lincoln, UK";
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ LearningMVC/Models/AboutModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LearningMVC.Models
+{
+    public class AboutModel
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
